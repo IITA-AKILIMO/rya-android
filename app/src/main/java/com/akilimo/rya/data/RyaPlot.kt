feb58d638b23b59c66bb3ad022a345bee837f678
Akilimo Machine <dev@tsobu.co.ke>
2022-02-21 15:13:18 +0300

    refactor: removed uneeded annotations

--- app/src/main/java/com/akilimo/rya/data/RyaPlot.kt
+++ app/src/main/java/com/akilimo/rya/data/RyaPlot.kt
@@ -4,10 +4,6 @@ import com.fasterxml.jackson.annotation.JsonAutoDetect
 import com.fasterxml.jackson.annotation.JsonProperty
 
 
-@JsonAutoDetect(
-    getterVisibility = JsonAutoDetect.Visibility.NONE,
-    fieldVisibility = JsonAutoDetect.Visibility.ANY
-)
 data class RyaPlot(
     @get:JsonProperty("file_name")
     val fileName: String = "device_20",
