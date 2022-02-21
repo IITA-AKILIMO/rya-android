b793d4f61b5217714bc1844b67babc3a179ca961
Akilimo Machine <dev@tsobu.co.ke>
2022-02-21 14:50:56 +0300

    feat: zoom library switch
    switched to using a zooming library

--- app/src/main/java/com/akilimo/rya/views/activities/HomeStepperActivity.kt
+++ app/src/main/java/com/akilimo/rya/views/activities/HomeStepperActivity.kt
@@ -1,54 +1,31 @@
 package com.akilimo.rya.views.activities
 
+import android.annotation.SuppressLint
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.graphics.Matrix
-import android.graphics.PointF
 import android.os.Bundle
-import android.util.Log
-import android.view.MotionEvent
-import android.view.View
-import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import com.akilimo.rya.data.RyaPlot
 import com.akilimo.rya.databinding.ActivityHomeStepperBinding
 import com.akilimo.rya.rest.ApiInterface
+import com.davemorrissey.labs.subscaleview.ImageSource
 import okhttp3.ResponseBody
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
 
-class HomeStepperActivity : AppCompatActivity(), View.OnTouchListener {
+class HomeStepperActivity : AppCompatActivity() {
     private lateinit var binding: ActivityHomeStepperBinding
 
 
-    private val TAG = "Touch"
-    private val MIN_ZOOM = 1f
-    private val MAX_ZOOM = 1f
-
-    // These matrices will be used to scale points of the image
-    var matrix = Matrix()
-    var savedMatrix = Matrix()
-
-    // The 3 states (events) which the user is trying to perform
-    val NONE = 0
-    val DRAG = 1
-    val ZOOM = 2
-    var mode = NONE
-
-    // these PointF objects are used to record the point(s) the user is touching
-    var start = PointF()
-    var mid = PointF()
-    var oldDist = 1f
-
+    @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_home_stepper)
         binding = ActivityHomeStepperBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        val image = binding.imageView
-        image.setOnTouchListener(this)
+        val imageView = binding.imageView
 
         val apiInterface =
             ApiInterface.create().readPlots(RyaPlot(fileName = "device_20"))
@@ -58,7 +35,7 @@ class HomeStepperActivity : AppCompatActivity(), View.OnTouchListener {
                 if (response.body() != null) {
                     val bytes = response.body()!!.bytes()
                     val bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.size)
-                    image.setImageBitmap(bitmap.rotate(-90f))
+                    imageView.setImage(ImageSource.bitmap(bitmap.rotate(-90f)))
                 }
             }
 
@@ -72,125 +49,5 @@ class HomeStepperActivity : AppCompatActivity(), View.OnTouchListener {
     fun Bitmap.rotate(degrees: Float): Bitmap {
         val matrix = Matrix().apply { postRotate(degrees) }
         return Bitmap.createBitmap(this, 0, 0, width, height, matrix, true)
-    }
-
-    override fun onTouch(v: View?, event: MotionEvent): Boolean {
-        val view: ImageView = v as ImageView
-        view.scaleType = ImageView.ScaleType.MATRIX
-        var scale: Float
-
-        dumpEvent(event)
-        // Handle touch events here...
-
-        when (event.action and MotionEvent.ACTION_MASK) {
-            MotionEvent.ACTION_DOWN -> {
-                matrix.set(view.imageMatrix)
-                savedMatrix.set(matrix)
-                start[event.x] = event.y
-                Log.d(TAG, "mode=DRAG") // write to LogCat
-                mode = DRAG
-            }
-            MotionEvent.ACTION_UP, MotionEvent.ACTION_POINTER_UP -> {
-                mode = NONE
-                Log.d(TAG, "mode=NONE")
-            }
-            MotionEvent.ACTION_POINTER_DOWN -> {
-                oldDist = spacing(event)
-                Log.d(TAG, "oldDist=$oldDist")
-                if (oldDist > 5f) {
-                    savedMatrix.set(matrix)
-                    midPoint(mid, event)
-                    mode = ZOOM
-                    Log.d(TAG, "mode=ZOOM")
-                }
-            }
-            MotionEvent.ACTION_MOVE -> if (mode == DRAG) {
-                matrix.set(savedMatrix)
-                matrix.postTranslate(
-                    event.getX() - start.x,
-                    event.getY() - start.y
-                ) // create the transformation in the matrix  of points
-            } else if (mode == ZOOM) {
-                // pinch zooming
-                val newDist = spacing(event)
-                Log.d(TAG, "newDist=$newDist")
-                if (newDist > 5f) {
-                    matrix.set(savedMatrix)
-                    scale = newDist / oldDist // setting the scaling of the
-                    // matrix...if scale > 1 means
-                    // zoom in...if scale < 1 means
-                    // zoom out
-                    matrix.postScale(scale, scale, mid.x, mid.y)
-                }
-            }
-        }
-        view.imageMatrix = matrix; // display the transformation on screen
-
-        return true
-    }
-
-
-    /*
-     * --------------------------------------------------------------------------
-     * Method: spacing Parameters: MotionEvent Returns: float Description:
-     * checks the spacing between the two fingers on touch
-     * ----------------------------------------------------
-     */
-
-    /*
-     * --------------------------------------------------------------------------
-     * Method: spacing Parameters: MotionEvent Returns: float Description:
-     * checks the spacing between the two fingers on touch
-     * ----------------------------------------------------
-     */
-    private fun spacing(event: MotionEvent): Float {
-        val x = event.getX(0) - event.getX(1)
-        val y = event.getY(0) - event.getY(1)
-        return Math.sqrt((x * x + y * y).toDouble()).toFloat()
-    }
-
-    /*
-     * --------------------------------------------------------------------------
-     * Method: midPoint Parameters: PointF object, MotionEvent Returns: void
-     * Description: calculates the midpoint between the two fingers
-     * ------------------------------------------------------------
-     */
-    private fun midPoint(point: PointF, event: MotionEvent) {
-        val x = event.getX(0) + event.getX(1)
-        val y = event.getY(0) + event.getY(1)
-        point[x / 2] = y / 2
-    }
-    /** Show an event in the LogCat view, for debugging  */
-    private fun dumpEvent(event: MotionEvent) {
-        val names = arrayOf(
-            "DOWN",
-            "UP",
-            "MOVE",
-            "CANCEL",
-            "OUTSIDE",
-            "POINTER_DOWN",
-            "POINTER_UP",
-            "7?",
-            "8?",
-            "9?"
-        )
-        val sb = StringBuilder()
-        val action = event.action
-        val actionCode = action and MotionEvent.ACTION_MASK
-        sb.append("event ACTION_").append(names[actionCode])
-        if (actionCode == MotionEvent.ACTION_POINTER_DOWN || actionCode == MotionEvent.ACTION_POINTER_UP) {
-            sb.append("(pid ").append(action shr MotionEvent.ACTION_POINTER_ID_SHIFT)
-            sb.append(")")
-        }
-        sb.append("[")
-        for (i in 0 until event.pointerCount) {
-            sb.append("#").append(i)
-            sb.append("(pid ").append(event.getPointerId(i))
-            sb.append(")=").append(event.getX(i).toInt())
-            sb.append(",").append(event.getY(i).toInt())
-            if (i + 1 < event.pointerCount) sb.append(";")
-        }
-        sb.append("]")
-        Log.d("Touch Events ---------", sb.toString())
     }
 }
