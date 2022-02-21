31cf19899a3bf9f29f3150f618cb6566747aba71
Akilimo Machine <dev@tsobu.co.ke>
2022-02-21 11:06:53 +0300

    feat: added stepper activity

---
+++ app/src/main/java/com/akilimo/rya/views/activities/HomeStepperActivity.kt
@@ -0,0 +1,12 @@
+package com.akilimo.rya.views.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.akilimo.rya.R
+
+class HomeStepperActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home_stepper)
+    }
+}
