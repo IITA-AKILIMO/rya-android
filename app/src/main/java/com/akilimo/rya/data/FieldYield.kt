7aff7399832fcccb0f5025127219780341b8dddc
Akilimo Machine <dev@tsobu.co.ke>
2022-03-01 15:48:29 +0300

    feat: added cardview for yield cassava classes

---
+++ app/src/main/java/com/akilimo/rya/data/FieldYield.kt
@@ -0,0 +1,9 @@
+package com.akilimo.rya.data
+
+data class FieldYield(
+    val imageId: Int,
+    val fieldYieldAmountLabel: String,
+    val yieldLabel: String,
+    val yieldAmount: Double,
+    val fieldYieldDesc: String
+)
