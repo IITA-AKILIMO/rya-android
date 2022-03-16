6a017922fd959e0233385c18221864a07399088e
Akilimo Machine <dev@tsobu.co.ke>
2022-03-16 10:30:37 +0300

    feat: added data validation
    added data validation on fragment steps

--- app/src/main/java/com/akilimo/rya/views/fragments/PlantingPeriodFragment.kt
+++ app/src/main/java/com/akilimo/rya/views/fragments/PlantingPeriodFragment.kt
@@ -1,11 +1,15 @@
 package com.akilimo.rya.views.fragments
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.akilimo.rya.R
+import com.akilimo.rya.databinding.FragmentFieldInfoBinding
+import com.akilimo.rya.databinding.FragmentPlantingPeriodBinding
+import com.stepstone.stepper.VerificationError
 
 
 /**
@@ -13,25 +17,49 @@ import com.akilimo.rya.R
  * Use the [PlantingPeriodFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class PlantingPeriodFragment : Fragment() {
+class PlantingPeriodFragment : BaseStepFragment() {
 
+    private var ctx: Context? = null
+    private var _binding: FragmentPlantingPeriodBinding? = null
+
+    private val binding get() = _binding!!
 
     companion object {
         @JvmStatic
         fun newInstance() = PlantingPeriodFragment()
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        this.ctx = context
     }
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_planting_period, container, false)
+        return loadFragmentLayout(inflater, container, savedInstanceState)
     }
 
 
+    override fun loadFragmentLayout(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentPlantingPeriodBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun verifyStep(): VerificationError? {
+        return null
+    }
+
+    override fun onSelected() {
+    }
+
+    override fun onError(error: VerificationError) {
+    }
 }
