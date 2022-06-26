028da76c63b4bfcec672309f6885db80b10836b9
Munywele Sammy <barsamms@gmail.com>
2022-06-26 14:02:04 +0300

    build: update kotlin KSP version

---
+++ app/src/main/java/com/akilimo/rya/views/activities/PlantTriangleStepperActivity.kt
@@ -0,0 +1,11 @@
+package com.akilimo.rya.views.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PlantTriangleStepperActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_plant_triangle_stepper)
+    }
+}
