0eed84855a3454c84bc6c263a3c3565d415a7193
Sammy M <s.barasa@cgiar.org>
2022-10-06 11:28:32 +0300

    feat: added saved data display
    added logic to display saved data

--- app/src/main/java/com/akilimo/rya/entities/FieldYieldEntity.kt
+++ app/src/main/java/com/akilimo/rya/entities/FieldYieldEntity.kt
@@ -6,16 +6,11 @@ import androidx.room.PrimaryKey
 
 @Entity(tableName = "field_yield")
 data class FieldYieldEntity(
-    @PrimaryKey(autoGenerate = true)
-    val id: Int? = null,
-    @ColumnInfo(name = "image_id")
-    val imageId: Int,
-    @ColumnInfo(name = "yield_amount_label")
-    val fieldYieldAmountLabel: String,
-    @ColumnInfo(name = "yield_label")
-    val yieldLabel: String,
-    @ColumnInfo(name = "yield_amount")
-    val yieldAmount: Double,
-    @ColumnInfo(name = "yield_desc")
-    val fieldYieldDesc: String
+    @PrimaryKey(autoGenerate = true) val id: Int? = null,
+    @ColumnInfo(name = "image_id") val imageId: Int,
+    @ColumnInfo(name = "yield_amount_label") val fieldYieldAmountLabel: String,
+    @ColumnInfo(name = "yield_label") val yieldLabel: String,
+    @ColumnInfo(name = "yield_amount") val yieldAmount: Double,
+    @ColumnInfo(name = "yield_desc") val fieldYieldDesc: String,
+    @ColumnInfo(name = "row_index") val rowIndex: Int
 )
