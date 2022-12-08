3e8e961c00813c1104c664abd8425b0d572cb9a7
Sammy M <s.barasa@cgiar.org>
2022-12-08 13:38:46 +0300

    refactor: updated font sizes
    updated font sizes to use preset values

---
+++ app/src/main/java/com/akilimo/rya/views/fragments/onboarding/OnboardingEighteenFragment.kt
@@ -0,0 +1,49 @@
+package com.akilimo.rya.views.fragments.onboarding
+
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.akilimo.rya.databinding.*
+import com.akilimo.rya.views.fragments.BaseStepFragment
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [OnboardingEighteenFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class OnboardingEighteenFragment : BaseStepFragment() {
+
+
+    private var ctx: Context? = null
+    private var _binding: FragmentOnboardingEighteenBinding? = null
+
+
+    private val binding get() = _binding!!
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of this fragment
+         *
+         * @return A new instance of fragment [OnboardingEighteenFragment].
+         */
+        @JvmStatic
+        fun newInstance() = OnboardingEighteenFragment()
+    }
+
+    override fun onAttach(_ctx: Context) {
+        super.onAttach(_ctx)
+        this.ctx = context
+    }
+
+
+    override fun loadFragmentLayout(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentOnboardingEighteenBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+}
