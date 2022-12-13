ae5241a35d28407f66b4cecd04bed462df66a352
Sammy M <s.barasa@cgiar.org>
2022-12-13 13:35:31 +0300

    feat: added date/yea picker

--- app/src/main/java/com/akilimo/rya/views/fragments/PlantingPeriodFragment.kt
+++ app/src/main/java/com/akilimo/rya/views/fragments/PlantingPeriodFragment.kt
@@ -1,20 +1,16 @@
 package com.akilimo.rya.views.fragments
 
-import android.app.Activity
 import android.app.DatePickerDialog
 import android.content.Context
-import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.DatePicker
-import androidx.appcompat.app.AppCompatDialogFragment
 import androidx.fragment.app.Fragment
 import com.akilimo.rya.databinding.FragmentPlantingPeriodBinding
 import com.akilimo.rya.utils.DateHelper
 import com.stepstone.stepper.VerificationError
-import java.text.DateFormatSymbols
+import com.whiteelephant.monthpicker.MonthPickerDialog
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -59,10 +55,8 @@ class PlantingPeriodFragment : BaseStepFragment() {
     }
 
     // create an OnDateSetListener
-    val dateSetListener = object : DatePickerDialog.OnDateSetListener {
-        override fun onDateSet(
-            view: DatePicker, year: Int, monthOfYear: Int, dayOfMonth: Int
-        ) {
+    private val dateSetListener =
+        DatePickerDialog.OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
             cal.set(Calendar.YEAR, year)
             cal.set(Calendar.MONTH, monthOfYear)
             cal.set(Calendar.DAY_OF_MONTH, dayOfMonth)
@@ -71,7 +65,6 @@ class PlantingPeriodFragment : BaseStepFragment() {
             val sdf = SimpleDateFormat(myFormat, Locale.US)
             binding.plantingYearMonth.text = sdf.format(cal.time)
         }
-    }
 
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -79,32 +72,43 @@ class PlantingPeriodFragment : BaseStepFragment() {
 
         val minDate = DateHelper.getMinDate(-16)
         val maxDate = DateHelper.getMinDate(0)
-
-        binding.btnDatePicker.setOnClickListener { vw ->
-            val dialog = DatePickerDialog(
-                vw.context, dateSetListener,
-                // set DatePickerDialog to point to today's date when it loads up
-                cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), cal.get(Calendar.DAY_OF_MONTH)
-            )
-
-            dialog.datePicker.minDate = minDate.timeInMillis
-            dialog.datePicker.maxDate = maxDate.timeInMillis
-            dialog.show()
+        val myFormat = "MM/yyyy" // mention the format you need
+        val sdf = SimpleDateFormat(myFormat, Locale.US)
 
 
-//            val dialog = DatePickerDialog(vw.context, { _, year, month, day_of_month ->
-//                cal.get(Calendar.YEAR)
-//                cal.get(Calendar.MONTH)
-//                cal.get(Calendar.DAY_OF_MONTH)
-//                val myFormat = "MM-yyyy"
-//                val sdf = SimpleDateFormat(myFormat, Locale.getDefault())
-//                binding.plantingYearMonth.text = sdf.format(cal.time)
-//            }, cal[Calendar.YEAR], cal[Calendar.MONTH], cal[Calendar.DAY_OF_MONTH])
-//            dialog.datePicker.minDate = minDate.timeInMillis
-//            dialog.datePicker.maxDate = cal.timeInMillis
-//            dialog.show()
+        val builder =
+            MonthPickerDialog.Builder(context, object : MonthPickerDialog.OnDateSetListener {
+                /**
+                 * @param selectedMonth The month that was set (0-11) for compatibility with [Calendar].
+                 * @param selectedYear  The year that was set.
+                 */
+                override fun onDateSet(selectedMonth: Int, selectedYear: Int) {
+                    cal.set(Calendar.YEAR, selectedYear)
+                    cal.set(Calendar.MONTH, selectedMonth)
 
+                    binding.plantingYearMonth.text = sdf.format(cal.time)
+                }
+
+            }, cal.get(Calendar.YEAR), cal.get(Calendar.MONTH))
+
+
+        val picker =
+            builder.setTitle("Select planting month").setMaxYear(maxDate.get(Calendar.YEAR))
+                .setMinYear(minDate.get(Calendar.YEAR)).build()
+
+        with(binding) {
+            btnDatePicker.setOnClickListener { picker.show() }
         }
+//        binding.btnDatePicker.setOnClickListener { vw ->
+//            val dialog = DatePickerDialog(
+//                vw.context, dateSetListener,
+//                // set DatePickerDialog to point to today's date when it loads up
+//                cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), cal.get(Calendar.DAY_OF_MONTH)
+//            )
+//            dialog.datePicker.minDate = minDate.timeInMillis
+//            dialog.datePicker.maxDate = maxDate.timeInMillis
+//            dialog.show()
+//        }
 
 
     }
